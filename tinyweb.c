a494325e0d1a49f9babd3bf08e6af0276694d64c
summerwind <1258664108@qq.com>
2016-08-01 22:18:44 +0800

    2016.8.1 22:18

--- tinyweb.c
+++ tinyweb.c
@@ -8,12 +8,24 @@
 #include <sys/socket.h>
 #include <string.h>
 #include <pthread.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+
+#define SERVER_STRING "Server: summerwind/0.1.0\r\n"
 
 int start_up(int port);
 void error_die(const char *msg);
 void deal_request(void *server_sock);
 int get_line(int,char *,int);
 void uncompleted(int,char *);
+void not_found(int,char *);
+void deal_cgi(int,char *,char *,char *);
+void deal_static(int,char *);
+void send_head(int,char *);
+void send_content(int,FILE *);
+void get_mime(char *content_type,char *path);
+void cgi_error(int);
+void invalid_req(int);
 
 int main(int argc,char **argv)
 {
@@ -103,6 +115,8 @@ void deal_request(void *client_sock){
 	char protocol[50];
 	bool cgi=false;
 	char *query_string=NULL;
+	char *path=NULL;
+	struct stat st;
 
 	charnum=get_line(cli_sock,buf,sizeof(buf));
 	if(charnum==0){
@@ -116,12 +130,12 @@ void deal_request(void *client_sock){
 	//method is not get and post
 	if((strcasecmp(method,"GET")!=0)&&(strcasecmp(method,"POST")!=0)){
 		
-		uncompleted(cli_sock,method);	
+		uncompleted(cli_sock,method);
+		return;
 	}
 
-	if(strcasecmp(method,"POST")==0){
-		
-	
+	if(strcasecmp(method,"POST")==0){	
+		cgi=true;
 	}
 
 	if(strcasecmp(method,"GET")==0){
@@ -131,14 +145,65 @@ void deal_request(void *client_sock){
 			*query_string='\0';
 			query_string++;
 		}
+		printf("url:%s\n query_stirng:%s\n",url,query_string);
 
 		charnum=get_line(cli_sock,buf,sizeof(buf));
-		while(charnum){
+		while((charnum>0)&&strcmp("\n",buf)){
+			//printf("%s",buf);
 			charnum=get_line(cli_sock,buf,sizeof(buf));
+
 		}
 	}
 
+	path=url+1;
+	if(stat(path,&st)==-1){
+		
+		//printf("path:%s\n",path);
+		not_found(cli_sock,path);
+	}
+	else{
+		if((st.st_mode&S_IFMT)==S_IFDIR){
+			sprintf(path,"index.html");
+		}
+
+		if((st.st_mode&S_IXOTH)||(st.st_mode&S_IXUSR)||(st.st_mode&S_IXGRP)){
+			cgi=true;
+		}
+
+		if(cgi){
+			deal_cgi(cli_sock,path,method,query_string);
+		}else{
+			deal_static(cli_sock,path);
+		}
+	
+	}
+
 	close(cli_sock);
+
+}
+
+void not_found(int client_sock,char *path){
+	
+	char buf[1024];
+	
+	sprintf(buf,"HTTP/1.0 404 NOT FOUND\r\n");
+	send(client_sock,buf,strlen(buf),0);
+	sprintf(buf,SERVER_STRING);
+	send(client_sock,buf,strlen(buf),0);
+	sprintf(buf,"Content-Type:text/html\r\n");
+	send(client_sock,buf,strlen(buf),0);
+	sprintf(buf,"\r\n");
+	send(client_sock,buf,strlen(buf),0);
+	sprintf(buf,"<html><title>Resource not found</title>\r\n");
+	send(client_sock,buf,strlen(buf),0);
+	sprintf(buf,"<body><h1>The resource of the path:\r\n");
+	send(client_sock,buf,strlen(buf),0);
+	sprintf(buf,"%s\n",path);
+	send(client_sock,buf,strlen(buf),0);
+	sprintf(buf,"not found\r\n");
+	send(client_sock,buf,strlen(buf),0);
+	sprintf(buf,"</h1></body></html>");
+	send(client_sock,buf,strlen(buf),0);
 
 }
 
@@ -214,4 +279,184 @@ void uncompleted(int client_sock,char *method){
 	sprintf(buf,"<body><p>This method :%s is not completed</p></body></html>\r\n",method);
 	send(client_sock,buf,strlen(buf),0);
 
+}
+
+void deal_static(int client_sock,char *path){
+	
+	//send_head(client_sock,path);
+
+	FILE *file=fopen(path,"r");
+	if(file==NULL){
+		not_found(client_sock,path);
+	}else{
+	
+		send_head(client_sock,path);
+		send_content(client_sock,file);
+	}
+
+	fclose(file);
+
+}
+
+void deal_cgi(int client_sock,char *path,char *method,char *query_string){
+	int charnum=-1;
+	char buf[1024];
+	int content_len=-1;
+	char *con_len;
+	int cgi_output[2];
+	int cgi_input[2];
+	pid_t pid;
+	char *content;
+	char c;
+	int status;
+
+	if(strcasecmp(method,"POST")==0){
+		charnum=get_line(client_sock,buf,sizeof(buf));
+		while(charnum>0 && strcmp(buf,"\n")){
+			buf[15]='\0';
+			if(strcmp(buf,"Content-Length:")==0){
+				content_len=atoi(&buf[16]);
+				con_len=&buf[16];
+			}
+
+			charnum=get_line(client_sock,buf,sizeof(buf));
+		}
+
+		if(content_len==-1){
+			invalid_req(client_sock);
+			return;
+		}
+
+	}
+
+	if(pipe(cgi_output)==-1){
+		cgi_error(client_sock);
+		return;
+	}
+
+	if(pipe(cgi_input)==-1){
+		cgi_error(client_sock);
+		return;
+	}
+
+	if(pid=fork()==-1){
+		cgi_error(client_sock);
+		return;
+	}
+
+	sprintf(buf,"HTTP/1.0 200 OK\r\n");
+	send(client_sock,buf,strlen(buf),0);
+
+	//child process
+	if(pid==0){
+		
+		dup2(cgi_input[0],0);
+		dup2(cgi_output[1],1);
+		close(cgi_input[1]);
+		close(cgi_output[0]);
+
+		setenv("REQUEST_METHOD",method,1);
+		if(strcasecmp(method,"GET")==0)
+			setenv("QUERY_STRING",query_string,1);
+		else
+			setenv("CONTENT_LENGTH",con_len,1);
+		
+		execl(path,path,NULL);
+		exit(0);
+	}else{
+		
+		close(cgi_input[0]);
+		close(cgi_output[1]);
+		
+		if(strcasecmp(method,"POST")==0){
+			
+			recv(client_sock,content,content_len,0);
+			write(cgi_input[1],content,content_len);
+			
+		}
+
+		while(read(cgi_output[0],&c,1)>0)
+			send(client_sock,&c,1,0);
+
+		close(cgi_output[0]);
+		close(cgi_input[1]);
+
+		waitpid(pid,&status,0);
+
+	}
+
+}
+
+void cgi_error(int client_sock){
+	
+	char buf[1024];
+
+	sprintf(buf,"HTTP/1.0 501 CGI EXEC ERROR\r\n");
+	sprintf(buf,"%sContent-Type:text/html\r\n\r\n",buf);
+	sprintf(buf,"%s<html><title>cgi error</title>\r\n",buf);
+	sprintf(buf,"%s<body><p>cgi exec error\r\n",buf);
+	sprintf(buf,"%s<p>maybe pipe or fork\r\n",buf);
+	sprintf(buf,"%s</body></html>\r\n",buf);
+	send(client_sock,buf,strlen(buf),0);
+
+}
+
+void invalid_req(int client_sock){
+
+	char buf[1024];
+
+	sprintf(buf,"HTTP/1.0 400 INVALID REQUEST\r\n");
+	sprintf(buf,"%sContent-Type:text/html\r\n\r\n",buf);
+	sprintf(buf,"%s<html><title>bad request</title>\r\n",buf);
+	sprintf(buf,"%s<body><p>this post method does not has content length",buf);
+	sprintf(buf,"%s</p></body></html>\r\n",buf);
+	send(client_sock,buf,strlen(buf),0);
+}
+
+void send_content(int client_sock,FILE *file){
+	char buf[1024];
+	fgets(buf,sizeof(buf),file);
+	while(!feof(file)){
+		send(client_sock,buf,strlen(buf),0);
+		fgets(buf,sizeof(buf),file);
+	}
+
+}
+
+void send_head(int client_sock,char *path){
+	
+	char content_type[30];
+	get_mime(content_type,path);
+	char buf[1024];
+	sprintf(buf,"HTTP/1.0 200 OK\r\n");
+	send(client_sock,buf,strlen(buf),0);
+
+	sprintf(buf,SERVER_STRING);
+	send(client_sock,buf,strlen(buf),0);
+
+	sprintf(buf,"Content-Type:%s\r\n",content_type);
+	send(client_sock,buf,strlen(buf),0);
+
+	sprintf(buf,"\r\n");
+	send(client_sock,buf,strlen(buf),0);
+}
+
+void get_mime(char *content_type,char *path){
+	char *index=strchr(path,'.');
+	
+	if((strcasecmp(index,".jpeg")==0)||(strcasecmp(index,".jpg")==0))
+		strcpy(content_type,"image/jpeg");
+	else if(strcasecmp(index,".pdf")==0)	
+		strcpy(content_type,"application/pdf");
+	else if(strcasecmp(index,".mp4")==0)
+		strcpy(content_type,"video/mp4");
+	else if(strcasecmp(index,".gif")==0)
+		strcpy(content_type,"image/gif");
+	else if(strcasecmp(index,".html")==0)
+		strcpy(content_type,"text/html");
+	else 
+		strcpy(content_type,"text/plain");
+	printf("content-type:%s\n",content_type);
+	return;
+	//return content_type;
 }
